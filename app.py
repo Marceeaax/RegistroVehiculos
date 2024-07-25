cbe67b24fdd2ef7f861d08723b151e11543cf909
cacereslujann <cacereslujan183@gmail.com>
2024-07-25 14:44:30 -0400

    centrar no coincidencias

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import unicodedata
 import os
 import re
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vehiculos.db'
